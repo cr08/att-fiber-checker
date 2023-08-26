fa32a51e59f2706c46801da34b23b115b5d3c30e
cr08 <Vchat20@gmail.com>
2023-08-26 13:39:17 -0400

    Updated script with Discord webhook and multiple address allowances
    Changes made:
    
    Added Discord webhook notification code. Allows for choosing Slack or Discord or both based on credentials included in config.json
    
    Allow for multiple addresses to be checked. Address info moved to addresses.json. Script loops through all addresses until done.
    
    Added debug code that shows debug messages on the terminal at each stage and also writes the raw json response from ATT's site to a local file for review.
    
    Changed behavior to NOT send a notification by default if fiber is not available. Added command line option to enable notifications when fiber is NOT available.
    
    TODO:
    
    Prettify Discord messages with more formatting

--- scraper.py
+++ scraper.py
@@ -1,57 +1,104 @@
 import json
 import requests
 import sys
-from slacker import Slacker
 import argparse
+from datetime import datetime
+from slacker import Slacker
+from discordwebhook import Discord
 
-parser = argparse.ArgumentParser(description='Checks the configured address for availability of '
-                                             'ATT Fiber and posts the result to slack')
+def cleanFilename(sourcestring,  removestring ="\`/<>\:\"\\|?*"):
+    return ''.join([c for c in sourcestring if c not in removestring])
 
-parser.add_argument('-c', metavar='<config file path>', dest='config', help='The location of your config file',
-                    required=True)
+parser = argparse.ArgumentParser(description='Checks the configured addresses for availability of '
+                                             'ATT Fiber and posts the result to Slack and/or Discord')
+
+parser.add_argument('-d', '--debug', action='store_true', dest='debug', help='Enables debug logging to stdout and saves full json response file.',
+                    required=False)
+
+parser.add_argument('-n', '--nofiber', required=False, action='store_true', dest='nofiber', help='Send notification to Slack/Discord if no fiber is available.')
 
 args = parser.parse_args()
 
-# Load up the config from our JSON file...
+nofiber_notify = args.nofiber
+debug = args.debug
+
+# Load config file for Slack/Discord credentials
 config = {}
-with open(args.config, 'r') as f:
+with open("config.json", 'r') as f:
     config = json.load(f)
 
-slack = Slacker(config['slack_key'])
+# Load address list json file
+add = {}
+with open("addresses.json", 'r') as f:
+    add = json.load(f)
 
 availability_url = 'https://www.att.com/services/shop/model/ecom/shop/view/unified/qualification/service' \
                    '/CheckAvailabilityRESTService/invokeCheckAvailability'
-json_data = {
-    'userInputZip': config['addr_zip'],
-    'userInputAddressLine1': config['addr_line'],
-    'mode': 'fullAddress',
-    'customer_type': 'Consumer',
-    'dtvMigrationFlag': False
-}
-headers = {'content-type': 'application/json'}
-fiber_avail = False
 
-try:
-    resp = requests.post(availability_url, data = json.dumps(json_data), headers = headers)
-    resp_json = json.loads(resp.text)
-    fiber_avail = resp_json['profile']['isGIGAFiberAvailable']
-except:
-    print("Unexpected error:", sys.exc_info()[0])
+for address in add['addresses']:
 
-msg = "Fiber isn't available yet :("
-txt = ":cry: :cry: :cry: :cry: :cry:"
-color = "#f44242"
+    json_data = {
+        'userInputZip': address['addr_zip'],
+        'userInputAddressLine1': address['addr_line'],
+        'mode': 'fullAddress',
+        'customer_type': 'Consumer',
+        'dtvMigrationFlag': False
+    }
+    headers = {'content-type': 'application/json'}
+    fiber_avail = False
 
-if fiber_avail:
-    msg = "Fiber is available!!"
-    txt = ":tada:"
-    color = "#33c653"
+    try:
+        resp = requests.post(availability_url, data = json.dumps(json_data), headers = headers)
+        if debug:
+            today = datetime.now()
+            iso_date = today.isoformat()
+            print("DEBIG: Writing json response file - " + cleanFilename(iso_date) + ".json")
+            with open(cleanFilename(iso_date) + ".json", "w") as f:
+                f.write(resp.text)
+        resp_json = json.loads(resp.text)
+        fiber_avail = resp_json['profile']['isGIGAFiberAvailable']
+    except:
+        print("Unexpected error:", sys.exc_info()[0])
 
-attachment = {
-    "text": txt,
-    "color": color
-}
+    if "slack" in config:
+        slack = Slacker(config['slack']['slack_key'])
 
-slack.chat.post_message(config['channel'], msg, as_user='AT&T Bot', username='attbot',
-                        attachments=[attachment]
-                        )
+        if fiber_avail:
+            msg = "Fiber is available!!"
+            txt = ":tada:"
+            color = "#33c653"
+            attachment = {"text": txt,"color": color}
+            slack.chat.post_message(config['slack']['slack_channel'], msg, as_user='AT&T Bot', username='attbot',attachments=[attachment])
+            if debug:
+                print("DEBUG: Sending Slack message that fiber is available")
+        elif nofiber_notify:
+            msg = "Fiber isn't available yet :("
+            txt = ":cry: :cry: :cry: :cry: :cry:"
+            color = "#f44242"
+            attachment = {"text": txt,"color": color}
+            slack.chat.post_message(config['slack']['slack_channel'], msg, as_user='AT&T Bot', username='attbot',attachments=[attachment])
+            if debug:
+                print("DEBUG: Sending Slack message that fiber is NOT available")
+        else:
+            if debug:
+                print("DEBUG: Fiber not available and 'nofiber' argument not set. Not sending message to Slack.")
+    else:
+        if debug:
+            print("DEBUG: Slack credentials not set in config. Skipping Slack notification pass.")
+
+    if "discord" in config:
+        disc_wh = Discord(url=config['discord']['webhook_url'])
+
+        if fiber_avail:
+            if debug:
+                print("DEBUG: Sending Discord message that fiber is available")
+            disc_wh.post(content="YAY! Fiber IS available at `" + address['addr_line'] + ", " + address['addr_zip'] + "`")
+        elif nofiber_notify:
+            if debug:
+                print("DEBUG: Sending Discord message that fiber is NOT available")
+            disc_wh.post(content="Fiber is NOT available at `" + address['addr_line'] + ", " + address['addr_zip'] + "`")
+        else:
+            if debug:
+                print("DEBUG: Fiber not available and 'nofiber' argument not set. Not sending message to Discord.")
+    else:
+        print("DEBIUG: Discord webhook url not set in config. Skipping Discord notification pass.")
